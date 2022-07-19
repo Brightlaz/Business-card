f8f7ab463a595a2b2fdd91530e0784a245a02075
Brightlaz <lazbright1@gmail.com>
2022-07-19 11:06:23 -0700

    Initial commit

---
+++ card/src/components/Picture.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import image from "../image.png"
+export default function Picture() {
+    return ( 
+        <img src ={image}
+        className = "image"
+        alt = "logo" />
+    )
+}
