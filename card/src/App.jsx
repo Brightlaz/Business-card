a72aa2b1cca7ffb09b0e6f6f3a832305efab1a0c
Bright Lazarus <lazbright1@gmail.com>
2022-09-13 16:27:49 +0100

    Synced the darkmode to the browser

--- card/src/App.jsx
+++ card/src/App.jsx
@@ -1,28 +1,36 @@
 import React from "react";
-import Info from './components/Info';
-import About from './components/About';
-import Interests from './components/Interests';
-import Footer from './components/Footer';
+import { useEffect } from "react";
+import Info from "./components/Info";
+import About from "./components/About";
+import Interests from "./components/Interests";
+import Footer from "./components/Footer";
 import Toggler from "./components/Toggler";
 export default function App() {
-  const [darkMode, setDarkMode] = React.useState(true)
-    
+  let switchMode;
+  const [darkMode, setDarkMode] = React.useState(switchMode);
+  console.log(switchMode);
+  useEffect(() => {
+    switchMode = darkMode;
+    const items = JSON.parse(localStorage.getItem("items"));
+    if (items) {
+      setDarkMode(items);
+    }
+  }, [toggleDarkMode]);
   function toggleDarkMode() {
-      setDarkMode(prevMode => !prevMode)
+    switchMode = !switchMode;
+    localStorage.setItem("items", JSON.stringify(switchMode));
+    setDarkMode((prevMode) => switchMode);
   }
-  
+
   return (
     <main className={darkMode ? "dark App" : "App"}>
-        <Info />
-        <div className="body">
-            <About />
-            <Interests />
-            <Toggler 
-            darkMode={darkMode} 
-            toggleDarkMode={toggleDarkMode}
-            />
-        </div>
-        <Footer  darkMode={darkMode} />
+      <Info />
+      <div className="body">
+        <About />
+        <Interests />
+        <Toggler darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      </div>
+      <Footer darkMode={darkMode} />
     </main>
   );
 }
